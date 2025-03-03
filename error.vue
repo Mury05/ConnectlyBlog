2d9df7ed24ac9eb371523a53a1253b9716c71436
Mury05 <murielleodilone@gmail.com>
2025-03-03 18:26:55 +0100

    Ajout d'une page d'erreur personnalisée

---
+++ error.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+
+</script>
+
+
+<template>
+    <div>
+        <AppHeader />
+        <UMain>
+            <UPage>
+                <UPageError :error="error" />
+            </UPage>
+        </UMain>
+        <AppFooter />
+    </div>
+
+</template>
+
+
+<style scoped></style>
