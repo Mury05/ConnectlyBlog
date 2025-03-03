f352f96f77507e44f3bcb5d49f18b826e705afc6
Mury05 <murielleodilone@gmail.com>
2025-03-03 09:58:09 +0100

    Ajout de la page d'index pour le blog avec affichage des articles

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('posts', () => queryCollection('blog').all());
+
+</script>
+
+
+<template>
+    <UContainer>
+        <UBlogList :posts="posts">
+            <UBlogPost v-for="post in posts" :key="post.id" :title="post.title" :description="post.description"
+                :image="post.image" :authors="post.auteurs" :date="formatDate(post.date)" :badge="{...post.badge, variant: 'outline'}" :to="post.path" />
+        </UBlogList>
+    </UContainer>
+</template>
+
+
+<style scoped></style>
