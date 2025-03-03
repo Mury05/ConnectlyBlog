082a3400a14207aeb979cc7f65857ac338cc74db
Mury05 <murielleodilone@gmail.com>
2025-03-03 14:34:11 +0100

    Ajout de la gestion du mode couleur et des métadonnées dans app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white' )
+useHead({
+  title: "Connectly",
+  htmlAttrs: {
+    lang: 'fr'
+  },
+  link: [
+    {rel: 'icon', href: '/favicon.ico'}
+  ],
+  meta: [
+    {charset: 'utf-8'},
+    {name: 'viewport', content: 'width=device, initial-scale=1.0'},
+    {name:'theme-color', content: color}
+  ]
+})
+</script>
 <template>
   <div>
     <NuxtLayout >
