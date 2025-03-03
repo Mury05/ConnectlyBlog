ba647d779c42884b07cecbb89f6bee6dbf9703ed
Mury05 <murielleodilone@gmail.com>
2025-03-03 09:57:52 +0100

    Ajout du default layout, du Header et du Footer

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <UMain>
+      <slot />
+    </UMain>
+    <AppFooter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
