02495cec89c27effc1478115409008f8f2ad67f9
Mury05 <murielleodilone@gmail.com>
2025-03-03 09:56:43 +0100

    Composant Header mis en place

---
+++ components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+const links = [
+    {
+        label: 'Blog',
+        to: '/blog',
+    },
+    {
+        label: 'A propos',
+        to: '/about',
+    }
+];
+
+</script>
+
+
+<template>
+
+    <UHeader>
+        <template #left>
+            <NuxtLink to="/">
+                <img src="/logo.png" alt="Logo de Connectly" class="w-auto h-6 shrink-0">
+            </NuxtLink>
+        </template>
+
+        <template #center>
+            <UHeaderLinks :links="links" class="hidden lg:flex" />
+        </template>
+
+        <template #right>
+            <div class="hidden lg:flex">
+
+                <UButton label="Se connecter" variant="link" color="orange" to="/login" />
+            </div>
+            <UColorModeButton />
+        </template>
+
+        <template #panel>
+                <UHeaderPopoverLinks :links="links" />
+                <UButton label="Se connecter" variant="link" color="orange" to="/login" />
+                <UColorModeButton />
+
+        </template>
+    </UHeader>
+
+</template>
+
+
+<style scoped></style>
